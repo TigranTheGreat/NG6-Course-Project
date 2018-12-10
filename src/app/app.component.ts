8c4d632b195b10d03c39ddc942d1a5e2093f2852
Tigran Grigoryan <tigranthegreatest@outlook.com>
2018-12-10 12:45:18 +0400

    Authentication

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import * as firebase from 'firebase';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   route = 'recipes';
+
+  ngOnInit() {
+    firebase.initializeApp({
+      apiKey: "AIzaSyCB8WdWitDW_7xOUJPk9TyAs5jX-U3ewcQ",
+      authDomain: "ng-recipe-book-bac28.firebaseapp.com"
+    })
+  }
 }
--- src/app/shared/data-storage.service.ts
+++ src/app/shared/data-storage.service.ts
@@ -3,6 +3,7 @@ import { Http, Response } from '@angular/http';
 import { RecipeService } from '../recipes/recipe.service';
 import { map } from 'rxjs/operators';
 import { Recipe } from '../recipes/recipe.model';
+import { AuthService } from '../auth/auth.service';
 
 @Injectable({
   providedIn: 'root'
@@ -10,15 +11,20 @@ import { Recipe } from '../recipes/recipe.model';
 export class DataStorageService {
   constructor(
     private http: Http,
-    private recipeService: RecipeService
+    private recipeService: RecipeService,
+    private authService: AuthService
   ) { }
 
   storeRecipes() {
-    return this.http.put('https://ng-recipe-book-bac28.firebaseio.com/recipes.json', this.recipeService.getRecipes());
+    let token = this.authService.getToken();
+
+    return this.http.put(`https://ng-recipe-book-bac28.firebaseio.com/recipes.json?auth=${token}`, this.recipeService.getRecipes());
   }
 
   getRecipes() {
-    return this.http.get('https://ng-recipe-book-bac28.firebaseio.com/recipes.json')
+    let token = this.authService.getToken();
+
+    return this.http.get(`https://ng-recipe-book-bac28.firebaseio.com/recipes.json?auth=${token}`)
       .pipe(
         map((response: Response) => {
           const recipes: Recipe[] = response.json();
